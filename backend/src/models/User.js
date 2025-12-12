6a090505326c5ced9d66f368290c4cff56d98f0b
keerthana03006 <s4392808@gmail.com>
2025-12-12 14:49:05 +0530

    maded some changes in the schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,33 +1,85 @@
+// const mongoose = require("mongoose");
+// const { v4: uuidv4 } = require("uuid");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     id: {
+//       type: String,
+//       default: uuidv4,
+//       unique: true,
+//     },
+//     first_name: {
+//       type: String,
+//       required: true,
+//     },
+//     last_name: {
+//       type: String,
+//       required: true,
+//     },
+//     phone_number: {
+//       type: String,
+//       required: true,
+//     },
+//     email_id: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     profile_picture: {
+//       type: String,
+//       default: "",
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("User", userSchema);
+
 const mongoose = require("mongoose");
-const { v4: uuidv4 } = require("uuid");
+const validator = require("validator");
 
 const userSchema = new mongoose.Schema(
   {
-    id: {
-      type: String,
-      default: uuidv4,
-      unique: true,
-    },
     first_name: {
       type: String,
       required: true,
+      trim: true,
     },
     last_name: {
       type: String,
       required: true,
+      trim: true,
     },
     phone_number: {
       type: String,
       required: true,
+      trim: true,
+      validate: {
+        validator: function (v) {
+          return /^[0-9]{10}$/.test(v);
+        },
+        message: "Phone number must be a 10-digit",
+      },
     },
     email_id: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+      validate: {
+        validator: validator.isEmail,
+        message: "Invalid email format",
+      },
     },
     password: {
       type: String,
       required: true,
+      minlength: 6,
     },
     profile_picture: {
       type: String,
@@ -38,3 +90,4 @@ const userSchema = new mongoose.Schema(
 );
 
 module.exports = mongoose.model("User", userSchema);
+
